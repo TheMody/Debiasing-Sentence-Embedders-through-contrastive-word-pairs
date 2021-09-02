4b7b8f0697bd12ca0248ab46a14adba25fe70903
Philip Kenneweg <pkenneweg@techfak.uni-bielefeld.de>
2021-09-02 14:32:29 +0200

    zeug

--- data.py
+++ data.py
@@ -45,13 +45,15 @@ def get_understanding_set(definition_pairs,tokenizer,max_examples = 20000):
 
 
 def load_pretrain_ds( batch_size = 8, max_length = 1024, min_length = 10):
-    dataset = tfds.load('multi_news', split="train", shuffle_files=False)
+    dataset = tfds.load('cnn_dailymail', split="train", shuffle_files=False)
     allsentences = []
     for newsarticle in dataset:
-        text = str(newsarticle["document"])
+        text = str(newsarticle["article"])
         for sentence in text.split("."):
             if len(sentence)>min_length & len(sentence)<max_length:
                 allsentences.append(sentence)
+        if len(allsentences) > 1000000:
+            break    
     new_ds = []
     new_batch = []
     for sentence in allsentences:
@@ -60,5 +62,7 @@ def load_pretrain_ds( batch_size = 8, max_length = 1024, min_length = 10):
             if len(new_batch)>= batch_size:
                 new_ds.append(new_batch)
                 new_batch = []
+#	if len(new_ds) > 1000000:
+#		break
     return new_ds
 
