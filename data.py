9eb7db2fcb40649cf199bfbe992310458ec2d437
Philip <p.kenneweg@unitybox.de>
2021-09-16 11:49:52 +0200

    zeug

--- data.py
+++ data.py
@@ -5,14 +5,14 @@ import numpy as np
 
 def find_all_occurence_and_replace(definition_pairs, max_examples = 5000, max_length = 512, min_length = 8):
     
-    dataset = tfds.load('ag_news_subset', split="train", shuffle_files=False)
+    dataset = tfds.load('multi_news', split="train", shuffle_files=False)
     definition_sentences = []
     rng = np.random.default_rng()
     for definition_words in definition_pairs:
         bias_sentence_pair1 = []
         bias_sentence_pair2 = []
         for newsarticle in dataset:
-            newsarticle = str(newsarticle["description"])
+            newsarticle = str(newsarticle["document"])
             for sentence in newsarticle.split("."):
               if len(sentence)>min_length & len(sentence)<max_length:
                   for i,equalword in enumerate(definition_words):
@@ -45,10 +45,10 @@ def get_understanding_set(definition_pairs,tokenizer,max_examples = 20000):
 
 
 def load_pretrain_ds( batch_size = 8, max_length = 1024, min_length = 10):
-    dataset = tfds.load('ag_news_subset', split="train", shuffle_files=False)
+    dataset = tfds.load('multi_news', split="train", shuffle_files=False)
     allsentences = []
     for newsarticle in dataset:
-        text = str(newsarticle["description"])
+        text = str(newsarticle["document"])
         for sentence in text.split("."):
             if len(sentence)>min_length & len(sentence)<max_length:
                 allsentences.append(sentence)
